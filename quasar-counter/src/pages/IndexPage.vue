3df9aa0dd18f87fd63ae44bbda49e01ba0ebc241
liz66-g <litib279@gmail.com>
2024-12-02 18:27:55 +0000

    Video 14min

--- quasar-counter/src/pages/IndexPage.vue
+++ quasar-counter/src/pages/IndexPage.vue
@@ -1,8 +1,21 @@
 <template>
-  <q-page class="flex flex-center text-black">  
-    <div class="rows full-width">
-      <div class="col text-center"></div>
-      <div class="col text-center text-h1">
+  <q-page class="flex flex-center text-black">
+    <div class="row">
+      <q-input 
+      input-class="text-center text-h5 text-black"
+      color="teal"
+      placeholder="counter" 
+      filled />
+    </div>  
+    <div class="rows full-width items-center">
+      <div class="col text-center">
+        <q-btn
+          icon="remove" 
+          size="xl"
+        round 
+        />
+      </div>
+      <div class="col text-center text-h2">
         23
       </div>
       <div class="col text-center">
@@ -13,8 +26,23 @@
         />
       </div>
     </div>
+    <div class="row">
+      <q-btn
+          icon="resart_alt" 
+          size="xl"
+        round 
+        />
+    </div>
   </q-page>
 </template>
+
+<style scoped>
+.q-page{
+  max-width: 700px;
+  margin: 0 auto;
+
+}
+</style>
 
 <script setup>
 
