ba9ab5da843e510e2833aaae9af51184fcba16b7
liz66-g <litib279@gmail.com>
2024-12-02 18:05:30 +0000

    Video 11min

---
+++ quasar-counter/src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center text-black">  
+    <div class="rows full-width">
+      <div class="col text-center"></div>
+      <div class="col text-center text-h1">
+        23
+      </div>
+      <div class="col text-center">
+        <q-btn
+          icon="add_circle_outline" 
+          size="xl"
+        round 
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+
+</script>
